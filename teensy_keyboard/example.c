9fe3b1d6b291c11a014e8d3fc0af2bf156fdb13f
Miron Pawlik <miron@live.com>
2015-11-08 10:55:42 +0100

    Create function that convert text into keys

--- teensy_keyboard/example.c
+++ teensy_keyboard/example.c
@@ -25,6 +25,7 @@
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <stdlib.h>
 #include "usb_keyboard.h"
 
 #define LED_CONFIG	(DDRD |= (1<<6))
@@ -32,37 +33,157 @@
 #define LED_OFF		(PORTD |= (1<<6))
 #define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
 
-uint8_t number_keys[10]=
-	{KEY_0,KEY_1,KEY_2,KEY_3,KEY_4,KEY_5,KEY_6,KEY_7,KEY_8,KEY_9};
+const int textLength = 23;
+char textToType[] = "Dupa\nok\n1903\n!@#$%^&*.,";
+uint8_t keysToPress[46];
 
-uint16_t idle_count=0;
+void assignKey(uint8_t* keys, int place, int keyNum, int modifier)
+{
+	keys[place*2] = keyNum;
+	keys[place*2+1] = modifier;
+}
 
-uint8_t cmd[]=
-	{KEY_C,KEY_M,KEY_D, KEYPAD_ENTER};
-
-uint8_t notepad[]=
-	{KEY_N, KEY_O, KEY_T, KEY_E, KEY_P, KEY_A, KEY_D, KEYPAD_ENTER};
-	
-uint8_t test[]=
+void convertStrningToKeys(char* text, uint8_t* keys, int size)
+{
+	for(int i =0; i<size; i++)
 	{
-		KEY_T,
-		KEY_E,
-		KEY_S,
-		KEY_T		
-	};
+		if(text[i] >= 97 && text[i]<=122)//a-z
+		{
+			assignKey(keys, i, text[i]-93, 0);
+		}
+		else if(text[i] >= 65 && text[i]<=90)//A-Z
+		{
+			assignKey(keys, i, text[i]-61, KEY_LEFT_SHIFT);
+		}
+		else if(text[i] >= 49 && text[i]<=57)//1-9
+		{
+			assignKey(keys, i, text[i]-19, 0);
+		}
+		else{
+			switch(text[i]){
+				case 10:
+					assignKey(keys, i, KEYPAD_ENTER, 0);
+					break;
+				case 32: //space
+					assignKey(keys, i, KEY_SPACE, 0);
+					break;
+				case 33: //!
+					assignKey(keys, i, KEY_1, KEY_LEFT_SHIFT);
+					break;
+				case 34: //"
+					assignKey(keys, i, KEY_QUOTE, KEY_LEFT_SHIFT);
+					break;
+				case 35: //#
+					assignKey(keys, i, KEY_3, KEY_LEFT_SHIFT);
+					break;
+				case 36: //$
+					assignKey(keys, i, KEY_4, KEY_LEFT_SHIFT);
+					break;
+				case 37: //%
+					assignKey(keys, i, KEY_5, KEY_LEFT_SHIFT);
+					break;
+				case 38: //&
+					assignKey(keys, i, KEY_7, KEY_LEFT_SHIFT);
+					break;
+				case 39: //'
+					assignKey(keys, i, KEY_QUOTE, 0);
+					break;
+				case 40: //(
+					assignKey(keys, i, KEY_9, KEY_LEFT_SHIFT);
+					break;
+				case 41: //)
+					assignKey(keys, i, KEY_0, KEY_LEFT_SHIFT);
+					break;
+				case 42: //*
+					assignKey(keys, i, KEY_8, KEY_LEFT_SHIFT);
+					break;
+				case 43: //+
+					assignKey(keys, i, KEY_EQUAL, KEY_LEFT_SHIFT);
+					break;
+				case 44: //,
+					assignKey(keys, i, KEY_COMMA, 0);
+					break;
+				case 45: //-
+					assignKey(keys, i, KEY_MINUS, 0);
+					break;
+				case 46: //.
+					assignKey(keys, i, KEY_PERIOD, 0);
+					break;
+				case 47: // /
+					assignKey(keys, i, KEY_SLASH, 0);
+					break;
+				case 48: // 0
+					assignKey(keys, i, KEY_0, 0);
+					break;
+				case 58: // :
+					assignKey(keys, i, KEY_SEMICOLON, KEY_LEFT_SHIFT);
+					break;
+				case 59: // ;
+					assignKey(keys, i, KEY_SEMICOLON, 0);
+					break;
+				case 60: // <
+					assignKey(keys, i, KEY_COMMA, KEY_LEFT_SHIFT);
+					break;
+				case 61: // =
+					assignKey(keys, i, KEY_EQUAL, 0);
+					break;
+				case 62: // >
+					assignKey(keys, i, KEY_PERIOD, KEY_LEFT_SHIFT);
+					break;
+				case 63: // ?
+					assignKey(keys, i, KEY_SLASH, KEY_LEFT_SHIFT);
+					break;
+				case 64: // @
+					assignKey(keys, i, KEY_2, KEY_LEFT_SHIFT);
+					break;
+				case 91: // [
+					assignKey(keys, i, KEY_LEFT_BRACE, 0);
+					break;
+				case 92: // \
+					assignKey(keys, i, KEY_BACKSLASH, 0);
+					break;
+				case 93: // ]
+					assignKey(keys, i, KEY_RIGHT_BRACE, 0);
+					break;
+				case 94: // ^
+					assignKey(keys, i, KEY_6, KEY_LEFT_SHIFT);
+					break;
+				case 95: // _
+					assignKey(keys, i, KEY_MINUS, KEY_LEFT_SHIFT);
+					break;
+				case 96: // `
+					assignKey(keys, i, KEY_TILDE, 0);
+					break;
+				case 123: // {
+					assignKey(keys, i, KEY_LEFT_BRACE, KEY_LEFT_SHIFT);
+					break;
+				case 124: // |
+					assignKey(keys, i, KEY_BACKSLASH, KEY_LEFT_SHIFT);
+					break;
+				case 125: // }
+					assignKey(keys, i, KEY_RIGHT_BRACE, KEY_LEFT_SHIFT);
+					break;
+				case 126: // ~
+					assignKey(keys, i, KEY_TILDE, KEY_LEFT_SHIFT);
+					break;
+				default:
+					assignKey(keys, i, KEY_L, KEY_LEFT_ALT);
+					break;
+			}
+		}
+	}
+}
 
-void writeme(uint8_t text[], int size){
-	for(int i =0; i<size; i++){
-		usb_keyboard_press(text[i], 0);
+void writeKeys(uint8_t* keys, int textSize)
+{
+	for(int i =0; i<textSize; i++){
+		usb_keyboard_press(keys[i*2], keys[i*2+1]);
 		_delay_ms(40);
 	}
 }
 
 int main(void)
 {
-	uint8_t b, d, mask, i, reset_idle;
-	uint8_t b_prev=0xFF, d_prev=0xFF;
-
 	// set for 16 MHz clock
 	CPU_PRESCALE(0);
 
@@ -82,39 +203,20 @@ int main(void)
 
 	// Wait an extra second for the PC's operating system to load drivers
 	// and do whatever it does to actually be ready for input
-	_delay_ms(4000);
+	_delay_ms(1000);
 
-	// Configure timer 0 to generate a timer overflow interrupt every
-	// 256*1024 clock cycles, or approx 61 Hz when using 16 MHz clock
-	// This demonstrates how to use interrupts to implement a simple
-	// inactivity timeout.
-	TCCR0A = 0x00;
-	TCCR0B = 0x05;
-	TIMSK0 = (1<<TOIE0);
-	
-	_delay_ms(500);
-	usb_keyboard_press(KEY_R, KEY_LEFT_GUI);
-	_delay_ms(500);
-	
-	writeme(cmd, 4);
-	_delay_ms(300);
-	writeme(notepad, 8);
-	_delay_ms(300);
-	writeme(test, 4);
+	// _delay_ms(500);
+	// usb_keyboard_press(KEY_R, KEY_LEFT_GUI);
+	// _delay_ms(500);
+	// 
+	convertStrningToKeys(textToType, keysToPress, textLength);
+	writeKeys(keysToPress, textLength);
+
+	// writeme(cmd, 4);
+	// _delay_ms(300);
+	// writeme(notepad, 8);
+	// _delay_ms(300);
+	// writeme(test, 4);
 
 	
 }
-
-// This interrupt routine is run approx 61 times per second.
-// A very simple inactivity timeout is implemented, where we
-// will send a space character.
-ISR(TIMER0_OVF_vect)
-{
-	idle_count++;
-	if (idle_count > 61 * 8) {
-		idle_count = 0;
-		//usb_keyboard_press(KEY_SPACE, 0);
-	}
-}
-
-
